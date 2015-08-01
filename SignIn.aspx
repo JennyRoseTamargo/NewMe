6381042cf6ba5ab09640ebaafbbc9a9bb64c190a
Jenny Rose Tamargo <rose.tamargo@gmail.com>
2015-08-01 09:53:10 +0800

    This is it!
    Log in code behind

---
+++ SignIn.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Title="Sign In" MasterPageFile="../Site.master" Theme="Default" 
+    Language="C#" AutoEventWireup="true" CodeFile="SignIn.aspx.cs" Inherits="ARMS.Security.SignIn"  %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="cphHead" Runat="server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="cpHcontent" Runat="server">
+	<div class="post">
+	    <h2 class="title">Sign In</h2>
+	    <br />
+	    <div class="entry">
+            <asp:Login ID="Login1" runat="server" OnAuthenticate="OnAuthenticate" OnLoggedIn="OnLoggedIn">
+                <LayoutTemplate>
+                    <table border="0" cellpadding="1" cellspacing="0" 
+                        style="border-collapse:collapse;">
+                        <tr>
+                            <td>
+                                <table border="0" cellpadding="0">
+                                    <tr>
+                                        <td colspan="2">Enter your user name and password to sign in.</td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <asp:Label ID="UserNameLabel" runat="server" AssociatedControlID="UserName">User Name:</asp:Label>
+                                        </td>
+                                        <td>
+                                            <asp:TextBox ID="UserName" runat="server"></asp:TextBox>
+                                            <asp:RequiredFieldValidator ID="UserNameRequired" runat="server" 
+                                                ControlToValidate="UserName" ErrorMessage="User Name is required." 
+                                                ToolTip="User Name is required." ValidationGroup="vgLogin">*</asp:RequiredFieldValidator>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <asp:Label ID="PasswordLabel" runat="server" AssociatedControlID="Password">Password:</asp:Label>
+                                        </td>
+                                        <td>
+                                            <asp:TextBox ID="Password" runat="server" TextMode="Password"></asp:TextBox>
+                                            <asp:RequiredFieldValidator ID="PasswordRequired" runat="server" 
+                                                ControlToValidate="Password" ErrorMessage="Password is required." 
+                                                ToolTip="Password is required." ValidationGroup="vgLogin">*</asp:RequiredFieldValidator>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td align="center" colspan="2" style="color:Red;">
+                                            <asp:Literal ID="FailureText" runat="server" EnableViewState="False"></asp:Literal>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td align="right" colspan="2">
+                                            <asp:Button ID="btnLogin" runat="server" CommandName="Login" Text="Sign In" 
+                                                ValidationGroup="vgLogin" />
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </LayoutTemplate>
+            </asp:Login>                                
+	    </div>
+    </div>
+</asp:Content>
+
+	
---
+++ SignIn.aspx.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ARMS.Security
+{
+	public partial class SignIn : System.Web.UI.Page 
+	{
+        bool isActive = true;
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+            if (Session["gUserType"] != null)
+			{
+                if (Session["gUserType"].ToString() == "1") // Admin
+                {
+                    Response.Redirect("../Resource/Resource.aspx");
+                }
+                else if (Session["gUserType"].ToString() == "2") // Staff
+                {
+                    Response.Redirect("../Booking/BookingHistory.aspx");
+                }
+			}		
+		}
+
+        /// <summary>
+        /// OnAuthenticate event handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void OnAuthenticate(object sender, AuthenticateEventArgs e)
+        {
+            bool Authenticated = false;
+            Authenticated = SiteSpecificAuthenticationMethod(Login1.UserName.Trim(), Login1.Password);
+            e.Authenticated = Authenticated;
+
+            //// *** FOR TESTING ***
+            //e.Authenticated = false;
+            //if (String.Compare(Login1.UserName, "foo", true) == 0 &&
+            //    String.Compare(Login1.Password, "bar", false) == 0)
+            //{
+            //    Session["gUserName"] = Login1.UserName;
+            //    e.Authenticated = true;
+            //}
+        }
+
+		/// <summary>
+		/// Verify username and password.
+        /// Set global session vars on successful authentication.
+		/// </summary>
+		/// <param name="UserName"></param>
+		/// <param name="Password"></param>
+		/// <returns></returns>
+		private bool SiteSpecificAuthenticationMethod(string UserName, string Password)
+		{
+			bool result = false;
+			string connStr = ConfigurationManager.ConnectionStrings["DatabaseRMSData1"].ToString();
+			SqlConnection dbConn = new SqlConnection(connStr);
+			dbConn.Open();
+
+			try
+			{
+                string query = @"select StaffId, Title, FirstName, LastName, UserType, IsActive from tblStaff 
+                                where UserName = '" + UserName + "' and UserPassword = '" + Password + "'";
+
+				SqlCommand cmdIns = new SqlCommand(query, dbConn);
+				SqlDataReader rdr = null;
+				rdr = cmdIns.ExecuteReader();
+
+				if (rdr.HasRows)
+				{
+					while (rdr.Read())
+					{
+						// Store logged in user info to session vars
+						Session["gUserName"] = UserName;
+						Session["gStaffId"] = rdr["StaffId"].ToString();
+                        Session["gFullName"] = rdr["Title"].ToString() + " " + rdr["FirstName"].ToString() + " " + rdr["LastName"].ToString();
+                        Session["gUserType"] = rdr["UserType"].ToString();
+                        isActive = Convert.ToBoolean(rdr["IsActive"]);
+					}
+					result = true;
+				}
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.ToString(), ex);
+			}
+			finally
+			{
+				dbConn.Close();
+			}
+
+			return result;
+		}
+				
+		/// <summary>
+		/// Redirect user to start page once authenticated.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		protected void OnLoggedIn(object sender, EventArgs e)
+		{
+            // If user account is deactivated
+            if (!isActive)
+            {
+                // Remove session vars
+                if (Session["gUserName"] != null)
+                {
+                    Session.Remove("gUserName");
+                }
+                if (Session["gUserType"] != null)
+                {
+                    Session.Remove("gUserType");
+                }
+                if (Session["gStaffId"] != null)
+                {
+                    Session.Remove("gStaffId");
+                }
+                if (Session["gFullName"] != null)
+                {
+                    Session.Remove("gFullName");
+                }
+                Session.Abandon();
+
+                Response.Redirect("Forbidden.aspx");
+            }
+            else
+            {
+
+                if (Session["gUserType"].ToString() == "1") // Admin
+                {
+                    Response.Redirect("../Resource/Resource.aspx");
+                }
+                else if (Session["gUserType"].ToString() == "2") // Staff
+                {
+                    Response.Redirect("../Booking/BookingHistory.aspx");
+                }
+            }
+		}
+	}
+}
